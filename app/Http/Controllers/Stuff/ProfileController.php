8f70cbc3fadeab80f93e3c211bb04dbe4467b958
kr95656 <katmsg8239@gmail.com>
2021-06-05 00:30:28 +0900

    リクエストフォームの追加

--- app/Http/Controllers/Stuff/ProfileController.php
+++ app/Http/Controllers/Stuff/ProfileController.php
@@ -3,15 +3,31 @@
 namespace App\Http\Controllers\Stuff;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\Stuff\Profile\EditRequest;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+
 
 class ProfileController extends Controller
 {
     public function showEditProfile ()
     {
         return view('stuffs.edit_profile_form')
-            ->with('employee', Auth::employee());
+            // ->with('employee', Auth::employee());
+            ->with('employee', Auth::user());
             // ->with('user', Auth::user());
     }
+
+    public function editProfile(EditRequest $request)
+    {
+        $user = Auth::user();
+
+        $user->name = $request->input('name');
+        $user->save();
+
+        return redirect()->back()
+            ->with('status', 'プロフィールを変更しました。');
+
+    }
+
 }
--- routes/web.php
+++ routes/web.php
@@ -19,12 +19,15 @@ Route::get('/', function () {
 
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/home', 'HomeController@index')
+    ->name('home');
 
 Route::prefix('stuff')
     ->namespace('Stuff')
     ->middleware('auth')
     ->group(function () {
         Route::get('edit-profile', 'ProfileController@showEditProfile')
+            ->name('stuffs.edit-profile');
+        Route::post('edit-profile', 'ProfileController@editProfile')
             ->name('stuffs.edit-profile');
     });
