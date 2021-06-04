79719478f42f63b8e5feb5f084f9c191d2c52005
kr95656 <katmsg8239@gmail.com>
2021-06-05 01:12:37 +0900

    画像加工ライブラリの追加

--- database/migrations/2021_05_30_192600_create_app_tables.php
+++ database/migrations/2021_05_30_192600_create_app_tables.php
@@ -66,6 +66,7 @@ class CreateAppTables extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('avatar_name')->nullable();
             $table->rememberToken();
             $table->timestamps();
 
